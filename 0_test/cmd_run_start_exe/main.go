174f9004770a0630b5db16b39548c2944feda41e
ck996 <1164537025@qq.com>
2023-04-15 17:03:30 +0800

    完善 函数

--- 0_test/cmd_run_start_exe/main.go
+++ 0_test/cmd_run_start_exe/main.go
@@ -1,92 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/axgle/mahonia"
 	"github.com/front-ck996/csy"
-	"os"
-	"os/exec"
 )
 
 func main() {
-	fmt.Println(os.Getwd())
-	fmt.Println(mahonia.NewDecoder("utf8").ConvertString("你好"))
-	// 创建一个命令
-	//cmd := exec.Command("cmd.exe", "/C", "C:\\Users\\Administrator\\Desktop\\xUltimate-d9pc-x86\\xUltimate-d9pc.exe")
-	cmd := exec.Command("cmd.exe", "/C", "aifgabfife ")
+	cmd := csy.Cmd{}
+	cmd.StreamStderrCB = func(text string) {
 
-	// 获取标准输入流
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		fmt.Println("获取标准输入流时发生错误：", err)
-		return
 	}
-
-	// 获取标准输出流
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println("获取标准输出流时发生错误：", err)
-		return
+	cmd.StreamStdinCB = func(text string) {
+		fmt.Println(text)
+		cmd.InputText("213")
 	}
+	run, err := cmd.Run([]string{
+		"G:\\code\\my\\csy\\0_test\\cmd_run_start_exe\\ask.exe",
+		//"ping", "-t", "baidu.com",
+	})
+	fmt.Println(run, err)
 
-	// 获取标准错误流
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		fmt.Println("获取标准错误流时发生错误：", err)
-		return
-	}
-
-	// 将标准输出流连接到当前程序的流
-	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			//fmt.Println("12345")
-			//fmt.Println(scanner.Text())
-			utf8, _ := csy.ConvCharsetToUtf8(scanner.Bytes())
-			//csy.ConvCharsetToUtf8(scanner.Text(), "")
-			fmt.Println(utf8)
-		}
-	}()
-
-	// 将标准错误流连接到当前程序的流
-	go func() {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			toUtf8, _ := csy.GbkToUtf8(scanner.Bytes())
-			fmt.Println(string(toUtf8))
-		}
-	}()
-
-	// 启动命令
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println("启动命令时发生错误：", err)
-		return
-	}
-
-	// 读取用户输入并发送到标准输入流中
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		_, err := fmt.Fprintln(stdin, text)
-		if err != nil {
-			fmt.Println("向标准输入流发送数据时发生错误：", err)
-			return
-		}
-	}
-
-	// 关闭标准输入流
-	err = stdin.Close()
-	if err != nil {
-		fmt.Println("关闭标准输入流时发生错误：", err)
-		return
-	}
-
-	// 等待命令完成
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Println("等待命令完成时发生错误：", err)
-		return
-	}
+	fmt.Println(cmd)
 }
---
+++ CmdCommand.go
@@ -0,0 +1,150 @@
+package csy
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/saintfish/chardet"
+	"io"
+	"os/exec"
+	"runtime"
+)
+
+// Cmd 封装常用的操作系统命令的函数
+type Cmd struct {
+	Origin *exec.Cmd
+	// 执行命令前，进入这个目录
+	RunBeforeCdDir string
+
+	// 执行过程中输出的流
+	StreamStdinCB func(text string)
+	// 执行过程中错误的流
+	StreamStderrCB func(text string)
+	Stdin          io.WriteCloser
+	Stdout         io.ReadCloser
+	StdoutText     string
+	Stderr         io.ReadCloser
+	StderrText     string
+}
+
+func (c *Cmd) convCharset(text string) string {
+
+	detector := chardet.NewTextDetector()
+	r, err := detector.DetectBest([]byte(text))
+	if r.Charset == "UTF-8" && err == nil {
+		return text
+	}
+	if c.isWindows() {
+		utf8, _ := GbkToUtf8([]byte(text))
+		return string(utf8)
+	}
+	return text
+}
+func (c *Cmd) isWindows() bool {
+	return runtime.GOOS == "windows"
+}
+
+func (c *Cmd) InputText(input string) error {
+	// 可以接受在系统过程中的输入，暂时不需要
+	//scanner := bufio.NewScanner(os.Stdin)
+	_, err := fmt.Fprintln(c.Stdin, input)
+	return err
+}
+
+func (c *Cmd) Close() error {
+	return c.Stdin.Close()
+}
+func (c *Cmd) Exit() {
+	c.Stdin.Close()
+	c.Origin.Process.Kill()
+	//SIGINT 通常用于中断进程，
+	//SIGTERM 通常用于正常终止进程，
+	//SIGQUIT 通常用于优雅退出进程。
+	//signal.Notify()
+	//err := c.Origin.Process.Signal(syscall.SIGQUIT)
+	//// 退出失败直接杀死进程
+	//if err != nil {
+	//	//err :=
+	//	//if err != nil {
+	//	//	fmt.Println("强制杀死进程失败", err)
+	//	//}
+	//	return
+	//}
+}
+
+func (c *Cmd) Run(inputCmd []string) (string, error) {
+	var cmdStr []string
+	switch {
+	case runtime.GOOS == "windows":
+		cmdStr = append(cmdStr, "cmd.exe", "/C")
+		break
+	default:
+		cmdStr = append(cmdStr, "hash", "-c")
+	}
+	cmdStr = append(cmdStr, inputCmd...)
+	cmd := exec.Command(cmdStr[0], cmdStr[1:]...)
+
+	c.Origin = cmd
+
+	// 获取输入流
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+	c.Stdin = stdin
+
+	// 获取输出流
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	c.Stdout = stdout
+
+	// 获取标准错误流
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+	c.Stderr = stderr
+
+	// 接受标准输出流
+	go func() {
+		scanner := bufio.NewScanner(c.Stdout)
+		for scanner.Scan() {
+			text := c.convCharset(scanner.Text())
+			if c.StreamStdinCB != nil {
+				c.StreamStdinCB(text)
+			}
+			c.StdoutText += text
+		}
+	}()
+
+	// 接受标准错误流
+	go func() {
+		scanner := bufio.NewScanner(c.Stderr)
+		for scanner.Scan() {
+			text := c.convCharset(scanner.Text())
+			if c.StreamStderrCB != nil {
+				c.StreamStderrCB(text)
+			}
+			c.StderrText += text
+		}
+	}()
+
+	// 启动命令
+	err = cmd.Start()
+	if err != nil {
+		return "", err
+	}
+	//// 关闭输入流
+	//err = stdin.Close()
+	//if err != nil {
+	//	return "", err
+	//}
+	// 等待命令执行完毕
+
+	err = cmd.Wait()
+	if err != nil {
+		return "", err
+	}
+	return "", nil
+}
