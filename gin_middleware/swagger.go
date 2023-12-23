d828341e5b6c1c672bd33849c945adb3faf15484
ck996 <you@example.com>
2023-12-24 01:01:42 +0800

    添加 gin_midd

---
+++ gin_middleware/swagger.go
@@ -0,0 +1,94 @@
+package middleware
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	//go:embed dist
+	front embed.FS
+)
+
+type SwaggerConfig struct {
+	RelativePath string
+}
+
+type swaggerService struct {
+	Name           string `json:"name"`
+	Url            string `json:"url"`
+	SwaggerVersion string `json:"swaggerVersion"`
+	Location       string `json:"location"`
+}
+type swaggerContent struct {
+	Swagger string `json:"swagger"`
+
+	Info struct {
+		Contact     struct{} `json:"contact"`
+		Description string   `json:"description"`
+		Title       string   `json:"title"`
+		Version     string   `json:"version"`
+	} `json:"info"`
+}
+
+func init() {
+	var err error
+	if err != nil {
+		log.Println("no swagger.json found in ./docs")
+	}
+}
+
+func SwaggerHandler(config SwaggerConfig) gin.HandlerFunc {
+
+	return func(ctx *gin.Context) {
+		if ctx.Request.Method != http.MethodGet {
+			ctx.AbortWithStatus(http.StatusMethodNotAllowed)
+			return
+		}
+
+		switch ctx.Request.URL.Path {
+		case config.RelativePath + "/docList":
+			var result []swaggerService
+			filepath.Walk("docs", func(path string, info fs.FileInfo, err error) error {
+				if strings.HasSuffix(path, ".json") {
+					parseDesc := swaggerContent{}
+					file, err := os.ReadFile(path)
+					if err == nil {
+						json.Unmarshal(file, &parseDesc)
+					}
+					result = append(result, swaggerService{
+						Name:           parseDesc.Info.Title,
+						SwaggerVersion: parseDesc.Swagger,
+						Url:            config.RelativePath + "/docJson?file=" + filepath.Base(path),
+						Location:       config.RelativePath + "/docJson?file=" + filepath.Base(path),
+					})
+				}
+				return nil
+			})
+			ctx.JSON(http.StatusOK, result)
+			break
+		case config.RelativePath + "/docJson":
+			docFile := path.Base(ctx.DefaultQuery("file", ""))
+			file, err := os.ReadFile("docs/" + docFile)
+			if err != nil {
+				return
+			}
+			ctx.JSON(200, string(file))
+			break
+
+		default:
+			fmt.Println(strings.TrimPrefix(ctx.Request.RequestURI, config.RelativePath), http.FS(front))
+			ctx.FileFromFS(strings.TrimPrefix(ctx.Request.RequestURI, config.RelativePath), http.FS(front))
+		}
+
+	}
+}
--- go.mod
+++ go.mod
@@ -14,26 +14,45 @@ require (
 
 require (
 	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/chromedp/cdproto v0.0.0-20231101223124-24f5925b5980 // indirect
 	github.com/chromedp/chromedp v0.9.3 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/parnurzeal/gorequest v0.2.16 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.etcd.io/bbolt v1.3.7 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
